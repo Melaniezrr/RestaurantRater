11c1cd442c79ce55669322041b41d3e80b61a68b
Melaniezrr <85641958+Melaniezrr@users.noreply.github.com>
2021-08-11 13:05:48 -0400

    Added Create method

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -16,5 +16,29 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
+
+        // Get: Create Restaurant
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                // SaveChanges() also returns a int with the count of how many rows were modified.
+                // We are not using this right now but it would be good to know for the future.
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
 }
